4d11b6fd0d2c7ea4528e4e98ab127152d7438d43
Chloie Parsons <chloieparsons@gmail.com>
2019-07-13 11:42:03 -0700

    File structure should be complete to the best of my foresight

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask
+
+from settings import NAME
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    print('Adam:', Adam)
+    f"NAME: {NAME}"
+    return render_template("hello.html", name=name)
+
+
+@app.route('/sales')
+def sales():
+    return render_template("sales.html", total=42)
